2a48f8faa2a6694859c2cb52fd420fa8ec07240a
Kushagrakatara89 <142562652+Kushagrakatara89@users.noreply.github.com>
2024-04-25 00:33:26 -0700

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,115 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How Many Avatars Did Lord Vishnu Have?",
+    answer: "10",
+    options: [
+      "10",
+      "4",
+      "7",
+      "11."
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which Is your Favourite Cricketer?",
+    answer: "Virat Kohli",
+    options: [
+      "MS Dhoni",
+      "Sachin Tendular",
+      "Virat Kohli",
+      "Yuvraj Singh"
+    ]
+  },
+  {
+    numb: 3,
+    question: "who will win 2024 election?",
+    answer: "sabhee chutiya hain",
+    options: [
+      "NDA Alliance",
+      "INDI Alliance",
+      "Both",
+      "sabhee chutiya hain",
+    
+    ]
+  },
+  {
+    numb: 4,
+    question: "Who Was The First Person To step foot on moon?",
+    answer: "KUSHAGRA KATARA",
+    options: [
+      "Niel Armstrong",
+      "MS dhoni ka ***",
+      "KUSHAGRA KATARA",
+      "Chhota Bheem"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Who wrote Mahabharat",
+    answer: "Ganesh ji",
+    options: [
+      "Ganesh ji",
+      "Valmiki",
+      "shivji",
+      "None of the above."
+    ]
+  },
+  // {
+  //   numb: 6,
+  //   question: "What does HTML stand for?",
+  //   answer: "Hyper Text Markup Language",
+  //   options: [
+  //     "Hyper Text Preprocessor",
+  //     "Hyper Text Markup Language",
+  //     "Hyper Text Multiple Language",
+  //     "Hyper Tool Multi Language"
+  //   ]
+  // },
+  // {
+  //   numb: 7,
+  //   question: "What does HTML stand for?",
+  //   answer: "Hyper Text Markup Language",
+  //   options: [
+  //     "Hyper Text Preprocessor",
+  //     "Hyper Text Markup Language",
+  //     "Hyper Text Multiple Language",
+  //     "Hyper Tool Multi Language"
+  //   ]
+  // },
+  // {
+  //   numb: 8,
+  //   question: "What does HTML stand for?",
+  //   answer: "Hyper Text Markup Language",
+  //   options: [
+  //     "Hyper Text Preprocessor",
+  //     "Hyper Text Markup Language",
+  //     "Hyper Text Multiple Language",
+  //     "Hyper Tool Multi Language"
+  //   ]
+  // },
+  // {
+  //   numb: 9,
+  //   question: "What does HTML stand for?",
+  //   answer: "Hyper Text Markup Language",
+  //   options: [
+  //     "Hyper Text Preprocessor",
+  //     "Hyper Text Markup Language",
+  //     "Hyper Text Multiple Language",
+  //     "Hyper Tool Multi Language"
+  //   ]
+  // },
+  // {
+  //   numb: 10,
+  //   question: "What does HTML stand for?",
+  //   answer: "Hyper Text Markup Language",
+  //   options: [
+  //     "Hyper Text Preprocessor",
+  //     "Hyper Text Markup Language",
+  //     "Hyper Text Multiple Language",
+  //     "Hyper Tool Multi Language"
+  //   ]
+  // },
+
+];
